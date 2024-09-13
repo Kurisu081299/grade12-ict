5009db1936a6dfa27f1ebef8c19e8923cbe3ef5e
CLIENT <bjbuendia1@gmail.com>
2024-09-13 14:22:23 +0800

    post method tutorial

--- buendia/Controller/userController.js
+++ buendia/Controller/userController.js
@@ -11,4 +11,18 @@ userController.getData = (req, res) => {
     });
 };
 
+userController.postData = (req, res) => {
+    const {name, address, age} = req.body;
+
+    const data = {name, address, age};
+
+    userModel.postData (data, (error,result) => {
+        if (error){
+            console.error("Error inserting data. Error: ", error);
+            return res.status(500).json({message: "Error inserting Data"});
+        }
+        return res.status (200).json({message: "Data inserted successfully"});
+    });
+}
+
 module.exports = userController;
