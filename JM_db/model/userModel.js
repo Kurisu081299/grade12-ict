2c19fb91b99855d399bc0bf91d31b1d9f20622c5
CLIENT <jeishanagustin@gmail.com>
2024-09-13 14:22:20 +0800

    post method tutorial

--- JM_db/model/userModel.js
+++ JM_db/model/userModel.js
@@ -2,7 +2,7 @@ const dbConn = require("../config/db.config");
 const userModel = {};
 
 userModel.getDataModel = (callback) => {
-    dbConn.query("SELECT * FROM jm_tb", (error,result) => {
+    dbConn.query("SELECT * FROM jei_table_1", (error,result) => {
         if(error){
             console.error("Error getting data: Data: ", error);
             return callback(error, null);
@@ -11,4 +11,13 @@ userModel.getDataModel = (callback) => {
     });
 };
 
+userModel.postData = (data, callback) =>{
+    dbConn.query("INSERT INTO jei_table_1 (name, address, age) VALUES (?, ?, ?)", [data.name, data.address, data.age], (error,result) => {
+        if(error){
+            console.error ("Error inserting data. ", error);
+            return callback (error, null);
+        }
+        return callback(null, result);
+    });
+}
 module.exports = userModel
